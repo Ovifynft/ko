30b66ea0fc54b5a94a7ad2c594f27010a7653254
Ovifynft <ovifynft@gmail.com>
2025-05-03 19:12:51 +0100

    stopped keeping track

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -2,6 +2,7 @@ import {gsap} from "../scripts/setup.ts"
 
 let dissappearBottomNav = gsap.fromTo('.bottom-nav', { opacity : 1}, {
     opacity : 0,
+    display : 'none',
     duration: 2,
     paused : true,
     ease: "circ.inOut"
